85ab2635ed1c9919d5add788263c0943243b9b56
Bruno Coelho <brunocoelho.ti@gmail.com>
2026-09-22 14:48:48 -0300

    __init__.py

--- BlogHashtagTreinamentos/__init__.py
+++ BlogHashtagTreinamentos/__init__.py
@@ -10,6 +10,7 @@ from sqlalchemy.future import engine
 
 app = Flask(__name__)
 
+## Teste
 
 app.config['UPLOAD_FOLDER'] = 'static/fotos_perfil'
 app.config['SECRET_KEY'] = '0d979629c3fe2692cc0a11969a070d99'  # Segurança do Formulários
