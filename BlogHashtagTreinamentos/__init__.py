ff8a0897bfd19e116411f1cabb5bff9cee669987
Bruno Coelho <brunocoelho.ti@gmail.com>
2026-09-21 22:40:19 -0300

    Atualiza __init__.py

--- BlogHashtagTreinamentos/__init__.py
+++ BlogHashtagTreinamentos/__init__.py
@@ -1,8 +1,11 @@
+import sqlalchemy
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+
+from sqlalchemy.future import engine
 
 app = Flask(__name__)
 
@@ -28,6 +31,18 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
+
+from BlogHashtagTreinamentos import models
+
+engine = sqlalchemy.create_engine(['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table ("usuario"):
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        print("Base de Dados Criada")
+else:
+    print("Base de Dados já Existente")
 
 
 from BlogHashtagTreinamentos import routes
